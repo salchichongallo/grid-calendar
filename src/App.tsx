90c70d7f7d033123c4738b8f78e79b030782365d
Jimmy Murillo <salchichongallo1997@gmail.com>
2023-02-16 19:18:13 -0500

    feat: init grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Calendar />
+    </Container>
+  );
+}
+
+const CalendarWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  outline: 5px solid greenyellow;
+`;
+
+const Row = styled.div`
+  border-bottom: 1px solid black;
+  flex: 1;
+`;
+
+const RowsContainer = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ColsContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+`;
+
+const Col = styled.div`
+  border-right: 1px solid black;
+  flex: 1;
+  position: relative;
+`;
+
+type SlotProps = {
+  $prefferedHeight: number;
+  $verticalOffset: number;
+};
+const Slot = styled.div<SlotProps>`
+  background: cyan;
+  height: ${(props) => props.$prefferedHeight}%;
+  position: absolute;
+  width: 100%;
+  left: 0;
+  top: ${(props) => props.$verticalOffset}%;
+  border-radius: 4px;
+`;
+
+const totalMinutes = 4 * 60;
+const durationInMinutes = 25; // 25 mins
+
+function Calendar() {
+  const height = (100 * durationInMinutes) / totalMinutes;
+  return (
+    <CalendarWrapper>
+      <RowsContainer>
+        <Row />
+        <Row />
+        <Row />
+        <Row />
+      </RowsContainer>
+      <ColsContainer>
+        <Col />
+        <Col>
+          <Slot $prefferedHeight={height} $verticalOffset={25}>
+            Hospital
+          </Slot>
+        </Col>
+        <Col />
+      </ColsContainer>
+    </CalendarWrapper>
   );
 }
 
